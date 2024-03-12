8b90eb78bb008946a009d8bb71e7802eb9a57e21
lucas_broda <lucas_broda@ens.univ-artois.fr>
2024-03-12 13:22:43 +0100

    feat(auth.ts): ajout des interfaces

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,18 +1,87 @@
 import { defineStore } from 'pinia';
 
+interface User {
+    int: number, 
+    firstName: string,
+    lastName: string,
+    email: string,
+    username: string,
+    roles: string[],
+    password: string,
+    contract: Contract[],
+    userGroup: UserGroup[],
+}
+
+interface UserGroup {
+    id: number,
+    groupName: string,
+    users: User[],
+    tasks: Task[], 
+    manager: User,
+}
+
+interface Task {
+    id: number,
+    name: string,
+    description: string,
+    estimatedTime: number,  
+    priority: Priority,
+    project: Project,
+    userGroup: UserGroup,
+}
+
+interface Priority {
+    id: number,
+    priority: string,
+    tasks: Task[],
+}
+
+interface Project {
+    id: number,
+    name: string,
+    color: string,
+    score: number,
+}
+
+interface Contract {
+    id: number, 
+    maximalWeeklyHours: number, 
+    openingHour: number, 
+    closingHour: number,
+    listUsers: User[],
+}
+
+interface Distribution {
+    id: number,
+    user: User,
+    morningStart: number,
+    morningEnd: number,
+    afternoonStart: number,
+    afternoonEnd: number,
+    date: Date,
+}
+
+interface Schedules {
+    id: number,
+    user: User,
+    task: Task,
+    timeSpent: number,
+    date: Date,
+}
+
 export const useAuthStore = defineStore('auth', {
     state: () => ({
         authenticated: false as boolean, 
         token: localStorage.getItem('item') ?? null,
-        project: null as any,
-        user: null as any,
-        task: null as any,
-        priority: null as any,
-        hours_worked: null as any,
-        group: null as any,
-        contract: null as any,
-        distribution: null as any,
-        schedules : null as any
+        project: null as Project | null,
+        user: null as User | null,
+        task: null as Task | null,
+        priority: null as Priority | null,
+        hours_worked: null as number | null,
+        group: null as UserGroup | null,
+        contract: null as Contract | null,
+        distribution: null as Distribution | null,
+        schedules : null as Schedules | null
     }),
 
     getters: {
@@ -20,45 +89,82 @@ export const useAuthStore = defineStore('auth', {
             return this.authenticated
         },
 
-        getProject(): any {
+        getProject(): Project | null{
             return this.project
         },
 
-        getToken(): any {
+        getToken(): string | null{
             return this.token
         },
 
-        getTask(): any {
+        getTask(): Task | null{
             return this.task
         },
 
-        getPriority(): any {
+        getPriority(): Priority | null{
             return this.priority
         },
 
-        getHoursWorked(): any {
+        getHoursWorked(): number | null{
             return this.hours_worked
         },
 
-        getUser(): any {
+        getUser(): User | null{
             return this.user
         },
 
-        getGroup(): any {
+        getUserGroup(): UserGroup | null{
             return this.group
         },
 
-        getContract(): any {
+        getContract(): Contract | null{
             return this.contract
         },
 
-        getDistribution(): any {
+        getDistribution(): Distribution | null{
             return this.distribution
         },
 
-        getSchedules(): any {
+        getSchedules(): Schedules | null{
             return this.schedules
+        }, 
+
+        getUserRole(): string[]{
+            return this.user?.roles ?? []
         }
 
     },
+
+    actions: {
+        async Contract(): Promise<void>{
+            const response = await fetch('http:localhost:8083/api/contracts', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${this.token}`,            
+                }
+            })
+
+            const responseData = await response.json()
+            if(responseData.status !== 200){
+                throw new Error(responseData.message || 'Failed to get contracts');
+            }
+            this.contract = responseData
+        }, 
+
+
+
+        async Distribution(): Promise<void>{
+            const response = await fetch('http:localhost:8083/api/distribution', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${this.token}`,            
+                }
+            })
+            const responseData = await response.json()
+            if(responseData.status !== 200){
+                throw new Error(responseData.message || 'Failed to get distribution');
+            }
+            this.distribution = responseData
+        },
+    }
 })
